2abf12ccc6a1880e78f56d751cd0a4f01ddcb3bb
Harry <copeharryv16@gmail.com>
2022-08-29 23:49:54 +0100

    i is teapot

---
+++ src/main/java/com/qa/qaCinema/QaCinemaOmdApplication.java
@@ -0,0 +1,13 @@
+package com.qa.qaCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaCinemaOmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaCinemaOmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/qaCinema/QaCinemaOmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.qaCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaCinemaOmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
