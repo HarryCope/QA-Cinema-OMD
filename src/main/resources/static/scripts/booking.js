89e7cf5cb955cdc6e23b85dd40ce349589c34277
Harry <copeharryv16@gmail.com>
2022-09-07 16:06:15 +0100

    oh lawd he coming

--- src/main/resources/static/scripts/booking.js
+++ src/main/resources/static/scripts/booking.js
@@ -21,11 +21,79 @@ fetch(`${bookingUrlBook}/getBooking`)
                 div.innerHTML = '<div style="padding-top:40px;"> Booking ID: ' + bookingData1[i].booking_Id +' </div><div>Film ID: ' + bookingData1[i].film_Id + '</div>' + ' Seat Numbers: ' + bookingData1[i].bookingSeatNumber + ' <div>Name: ' + bookingData1[i].bookingName + '</div>' + ' Screen Number: ' + bookingData1[i].bookingScreen + '  &nbsp &nbspBooking Time: ' 
                 + bookingData1[i].bookingTime + '<div> Price: £' + bookingData1[i].bookingPrice + '</div> <div style="padding-bottom:20px;"><button data-bs-toggle="modal" data-bs-target="#updateBookingModal" onclick="updateBooking(this.value)" value="' + bookingData1[i].booking_Id + '" id="update' + bookingData1[i].booking_Id + 
                 '" type="button" class="btn btn-secondary">Update</button> <button  onclick="deleteBooking(this.value)" value="' + bookingData1[i].booking_Id + '" id="delete' + bookingData1[i].booking_Id + '" type="button" class="btn btn-secondary">Delete</button></div>';
+                `<div class="col-6 col-lg-12">
+                    <div class="modal fade" id="updateBookingModal" tabindex="-1" aria-labelledby="bookingModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="bookingModalLabel" style="color: black;">Update Booking</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        <form>
+                                            <label for="updatedBookingName" class="form-label mt-2" style="color: black;">New Name</label>
+                                            <input type="text" class="form-control" id="updatedPokemonName">
+                                            <label for="updatedBookingTime" class="form-label mt-2" style="color: black;">New Time</label>
+                                            <input type="text" class="form-control" id="updatedPokemonName">
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-outline-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" id=" " onclick="updateBooking()"
+                                        data-bs-dismiss="modal">Update</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>`
                 mainContainer.appendChild(div);
 
             }
             
 }
+
+`<div class="col-6 col-lg-12">
+
+
+<div class="modal fade" id="updateBookingModal" tabindex="-1" aria-labelledby="bookingModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="bookingModalLabel" style="color: black;">Update Booking</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form>
+                        <label for="updatedBookingName" class="form-label mt-2" style="color: black;">New Name</label>
+                        <input type="text" class="form-control" id="updatedPokemonName">
+                        <label for="updatedBookingTime" class="form-label mt-2" style="color: black;">New Time</label>
+                        <input type="text" class="form-control" id="updatedPokemonName">
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary"
+                    data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id=" " onclick="updateBooking()"
+                    data-bs-dismiss="modal">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>`
+
+
+
 
 const deleteBooking = (val) => {
     let bookingID = val;
