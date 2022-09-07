fe25b39a8edadf038a35746483a5e83ae02d5b33
Danny Maksoud <dannymaksoud@gmail.com>
2022-09-07 14:58:04 +0100

    I have done what no man before me could do

--- src/main/resources/static/scripts/booking.js
+++ src/main/resources/static/scripts/booking.js
@@ -18,15 +18,15 @@ fetch(`${bookingUrlBook}/getBooking`)
                 let div = document.createElement("div");
                 div.innerHTML = '<div style="padding-top:40px;"> Booking ID: ' + bookingData1[i].booking_Id +' </div><div>Film ID: ' + bookingData1[i].film_Id + '</div>' + ' Seat Numbers: ' + bookingData1[i].bookingSeatNumber + ' <div>Name: ' + bookingData1[i].bookingName + '</div>' + ' Screen Number: ' + bookingData1[i].bookingScreen + '  &nbsp &nbspBooking Time: ' 
                 + bookingData1[i].bookingTime + '<div> Price: £' + bookingData1[i].bookingPrice + '</div> <div style="padding-bottom:20px;"><button id="update' + bookingData1[i].booking_Id + 
-                '" type="button" class="btn btn-secondary">Update</button> <button  onclick="deleteBooking()" value="' + bookingData1[i].booking_Id + '" id="delete' + bookingData1[i].booking_Id + '" type="button" class="btn btn-secondary">Delete</button></div>';
+                '" type="button" class="btn btn-secondary">Update</button> <button  onclick="deleteBooking(this.value)" value="' + bookingData1[i].booking_Id + '" id="delete' + bookingData1[i].booking_Id + '" type="button" class="btn btn-secondary">Delete</button></div>';
                 mainContainer.appendChild(div);
-                bookingID = bookingData1[i].booking_Id;
+
             }
             
 }
 
-const deleteBooking = () => {
-    
+const deleteBooking = (val) => {
+    let bookingID = val;
     fetch(`${bookingUrlBook}/deleteBooking/${bookingID}`, {
         method: "DELETE",
         headers: {
